d838d473a497d1d396034fa3447cab244806b035
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-30 09:39:24 +0200

    add text

--- app/src/main/java/com/example/admintasks/MainActivity.kt
+++ app/src/main/java/com/example/admintasks/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,7 +18,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.admintasks.ui.theme.AdminTasksTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,16 +51,29 @@ fun AdminTasks() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         GreetingImage()
+        GreetingText()
     }
 }
 
 @Composable
 fun GreetingImage() {
     val image = painterResource(R.drawable.ic_task_completed)
-
     Image (
         painter = image,
         contentDescription = null
+    )
+}
+
+@Composable
+fun GreetingText() {
+    Text(
+        text = "All tasks completed",
+        modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+        fontWeight = FontWeight.Bold
+    )
+    Text(
+        text = "Nice work!",
+        fontSize = 16.sp
     )
 }
 
