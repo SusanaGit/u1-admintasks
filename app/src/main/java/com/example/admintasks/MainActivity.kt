cf314c007c81054be3cc9e31a1d82895092b74f5
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-30 10:11:55 +0200

    string resources

--- app/src/main/java/com/example/admintasks/MainActivity.kt
+++ app/src/main/java/com/example/admintasks/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,12 +68,12 @@ fun GreetingImage() {
 @Composable
 fun GreetingText() {
     Text(
-        text = "All tasks completed",
+        text = stringResource(R.string.all_tasks_completed),
         modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
         fontWeight = FontWeight.Bold
     )
     Text(
-        text = "Nice work!",
+        text = stringResource(R.string.nice_work),
         fontSize = 16.sp
     )
 }
